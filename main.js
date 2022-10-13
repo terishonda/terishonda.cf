45117e88d98049329545544e4fa021ec34699c09
terishonda <115736016+terishonda@users.noreply.github.com>
2022-10-13 22:50:09 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+    document.getElementById("telegram").addEventListener("change", function(){
+    document.getElementById('tglink').innerHTML = "<a href='"+this.value + "'>"+this.value+"</a>";
+    });
